84b85b1bcadff2929279b9404d4d599d7c4424a4
sanja_s <sanja100v@gmail.com>
2023-12-01 08:54:27 +0300

    Add class MyClass

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 import time
 from urllib.request import urlopen
 
-url = "https://www.rbc.ru/"
+class MyClass():
+    url = "https://www.rbc.ru/"
+    start_title = "main__big__title"
+    end_title = "</title>"
+    file_name = ""
 
-def get_title(url):
-    
-    page = urlopen(url)
+    def __init__(self, name, age):
+        pass
 
-    # print(page)
+    def get_title(url):
+        
+        page = urlopen(url)
 
-    html_byte = page.read()
-    html = html_byte.decode("utf-8")
+        # print(page)
 
-    # print(html)
+        html_byte = page.read()
+        html = html_byte.decode("utf-8")
 
-    start_head = "<title>"
-    end_head = "</title>"
+        print(len(html))
 
-    title_index = html.find(start_head)
+        title_index = html.find(start_title)
 
-    start_index = title_index + len(start_head)
-    # print(start_index)
+        start_index = title_index + len(start_title)
+        print(start_index)
 
-    end_index = html.find(end_head)
-    # print(end_index)
+        end_index = html.find(end_title)
+        # print(end_index)
 
-    title = html[start_index:end_index]
-    
-    return title
+        title = html[start_index:]
+        
+        return title
+
+    def write_to_file():
+        pass
 
 
 if __name__ == "__main__":
-    while True:
+    # while True:
         title = get_title(url)
-        print(title)
-        time.sleep(1)
+        
+        # print(title)
+        # time.sleep(1)
